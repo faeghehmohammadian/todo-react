f300b87758852be9a62b2cb1735e58c6ea8173cb
Faegheh <59620602+faeghehmohammadian@users.noreply.github.com>
2023-12-05 11:16:45 +0400

    initial app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const TodoApp = () => {
+  const [todos, setTodos] = useState([]);
+  const [filter, setFilter] = useState('all');
+
+  const addTodo = (text) => {
+    setTodos([...todos, { id: Math.random(), text, completed: false }]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, completed: !todo.completed };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
+  const removeTodo = (id) => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
+
+  const clearCompletedTodos = () => {
+    setTodos(todos.filter((todo) => !todo.completed));
+  };
+
+  const handleFilterChange = (newFilter) => {
+    setFilter(newFilter);
+  };
+
+  const filteredTodos = todos.filter((todo) => {
+    if (filter === 'all') return true;
+    if (filter === 'active') return !todo.completed;
+    if (filter === 'completed') return todo.completed;
+    return false;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div className="todo-app">
+      <h1>Todo App</h1>
+
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        const text = e.target.elements.todoInput.value;
+        if (text) {
+          addTodo(text);
+          e.target.elements.todoInput.value = '';
+        }
+      }}>
+        <input type="text" placeholder="Enter a todo" id="todoInput" />
+        <button type="submit">Add Todo</button>
+      </form>
+
+      <div className="filters">
+        <button
+          className={filter === 'all' ? 'active' : ''}
+          onClick={() => handleFilterChange('all')}
         >
-          Learn React
-        </a>
-      </header>
+          All
+        </button>
+        <button
+          className={filter === 'active' ? 'active' : ''}
+          onClick={() => handleFilterChange('active')}
+        >
+          Active
+        </button>
+        <button
+          className={filter === 'completed' ? 'active' : ''}
+          onClick={() => handleFilterChange('completed')}
+        >
+          Completed
+        </button>
+      </div>
+
+      <ul>
+        {filteredTodos.map((todo) => (
+          <li key={todo.id} className={todo.completed ? 'completed' : ''}>
+            <input
+              type="checkbox"
+              checked={todo.completed}
+              onChange={() => toggleTodo(todo.id)}
+            />
+            {todo.text}
+            <button onClick={() => removeTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+
+      <button onClick={clearCompletedTodos}>Clear Completed Todos</button>
     </div>
   );
-}
+};
 
-export default App;
+export default TodoApp;
