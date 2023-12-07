6a2f704d4b418eb33feb564975102742b0555f09
Faegheh <59620602+faeghehmohammadian@users.noreply.github.com>
2023-12-07 12:35:17 +0400

    add edit function

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { useState } from 'react';
 import './App.css';
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTrash } from "@fortawesome/free-solid-svg-icons";
-import { faEdit } from "@fortawesome/free-solid-svg-icons";
+import { faTrash, faEdit, faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const TodoApp = () => {
   const [todos, setTodos] = useState([]);
   const [filter, setFilter] = useState('all');
+  const [editingTodoId, setEditingTodoId] = useState(null);
+  const [editedTodoText, setEditedTodoText] = useState('');
 
   const addTodo = (text) => {
     setTodos([...todos, { id: Math.random(), text, completed: false }]);
@@ -28,8 +29,31 @@ const TodoApp = () => {
     setTodos(todos.filter((todo) => todo.id !== id));
   };
 
+  const startEditing = (id, text) => {
+    setEditingTodoId(id);
+    setEditedTodoText(text);
+  };
+
+  const cancelEditing = () => {
+    setEditingTodoId(null);
+    setEditedTodoText('');
+  };
+
+  const saveEditedTodo = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          return { ...todo, text: editedTodoText };
+        } else {
+          return todo;
+        }
+      })
+    );
+    setEditingTodoId(null);
+    setEditedTodoText('');
+  };
+
   function handleFilterChange(value) {
-    // Update the filter state based on the value
     setFilter(value);
   }
 
@@ -43,49 +67,67 @@ const TodoApp = () => {
   return (
     <div className="todo-app">
       <h1>My Todos</h1>
-      <form className='formClass' onSubmit={(e) => {
-        e.preventDefault();
-        const text = e.target.elements.todoInput.value;
-        if (text) {
-          addTodo(text);
-          e.target.elements.todoInput.value = '';
-        }
-      }}>
+      <form
+        className='formClass'
+        onSubmit={(e) => {
+          e.preventDefault();
+          const text = e.target.elements.todoInput.value;
+          if (text) {
+            addTodo(text);
+            e.target.elements.todoInput.value = '';
+          }
+        }}
+      >
         <input type="text" placeholder="Enter a todo" id="todoInput" />
         <button type="submit">Save</button>
       
 
-      <div className="filters">
-        <select value={filter} onChange={(e) => handleFilterChange(e.target.value)}>
-          <option value="all" className={filter === 'all' ? 'active' : ''}>
-            All
-          </option>
-          <option value="active" className={filter === 'active' ? 'active' : ''}>
-            Active
-          </option>
-          <option value="completed" className={filter === 'completed' ? 'active' : ''}>
-            Completed
-          </option>
-        </select>
-      </div>
+        <div className="filters">
+          <select value={filter} onChange={(e) => handleFilterChange(e.target.value)}>
+            <option value="all" className={filter === 'all' ? 'active' : ''}>
+              All
+            </option>
+            <option value="active" className={filter === 'active' ? 'active' : ''}>
+              Active
+            </option>
+            <option value="completed" className={filter === 'completed' ? 'active' : ''}>
+              Completed
+            </option>
+          </select>
+        </div>
       </form>
-        {filteredTodos.map((todo) => (
-          <div style={{ width: '100%' }} key={todo.id} className={todo.completed ? 'completed' : ''}>
+
+      {filteredTodos.map((todo) => (
+        <div style={{ width: '90%' }} key={todo.id} className={todo.completed ? 'completed' : ''}>
+          {editingTodoId === todo.id ? (
+            <div className='todoRowBox'>
+              <input
+                type="text"
+                value={editedTodoText}
+                onChange={(e) => setEditedTodoText(e.target.value)}
+              />
+              <button className='confirmEditButton' onClick={() => saveEditedTodo(todo.id)}>
+                <FontAwesomeIcon icon={faCheck} />
+              </button>
+              <button className='cancelEditButton' onClick={() => cancelEditing()}>
+                <FontAwesomeIcon icon={faTimes} />
+              </button>
+            </div>
+          ) : (
             <div className='todoRowBox'>
               <input type="checkbox" checked={todo.completed} onChange={() => toggleTodo(todo.id)}/>
               <div className='todoRowText'>{todo.text}</div>
-              <button className='editButton' onClick={() => removeTodo(todo.id)}>
+              <button className='editButton' onClick={() => startEditing(todo.id, todo.text)}>
                 <FontAwesomeIcon icon={faEdit} />
               </button>
               <button className='deleteButton' onClick={() => removeTodo(todo.id)}>
                 <FontAwesomeIcon icon={faTrash} />
               </button>
             </div>
-          </div>
-        ))}
-      </div>
-
-    
+          )}
+        </div>
+      ))}
+    </div>
   );
 };
 
